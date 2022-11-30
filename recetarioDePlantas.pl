cf6f86421965fe151d249a9d1bfacc9065f1cf8b
MartinReyesG <s19120156@alumnos.itsur.edu.mx>
2022-11-29 22:31:23 -0600

    Se añadieron más datos como los modos de preparación, métodos de preparación. y paretes de las plantas y algunas reglas

--- recetarioDePlantas.pl
+++ recetarioDePlantas.pl
@@ -1,3 +1,9 @@
+
+%retama=esparteína
+%rosal=vitamina C, qurcitrina
+
+%¿Cuales son plantas o plantas medicinales?
+%*******************************************************************************************
 planta(retema).
 planta(ricino).
 planta(rosal).
@@ -10,65 +16,38 @@ planta(taray).
 planta(tila).
 
 
-nombreCientifico(retema,spartium_junceum).
-nombreCientifico(ricino,ricinus_communis).
-nombreCientifico(rosal,rosa_centifolia).
-nombreCientifico(romero,salvia_rosmarinus).
-nombreCientifico(ruda,ruta_graveolens).
-nombreCientifico(simonillo,laennecia_schiedeana).
-nombreCientifico(tamarindo,tamarindus_indica).
-nombreCientifico(tabachín,cesalpinea_pulcherrima).
-nombreCientifico(taray,amarix_gallica).
-nombreCientifico(tila,tilia_europea).
+%Listar plantas medicinales y su nombre científico
+%*******************************************************************************************
+nombreCientifico(retema,'spartium junceum').
+nombreCientifico(ricino,'ricinus communis').
+nombreCientifico(rosal,'rosa centifolia').
+nombreCientifico(romero,'salvia rosmarinus').
+nombreCientifico(ruda,'ruta graveolens').
+nombreCientifico(simonillo,'laennecia schiedeana').
+nombreCientifico(tamarindo,'tamarindus indica').
+nombreCientifico(tabachín,'cesalpinea pulcherrima').
+nombreCientifico(taray,'amarix gallica').
+nombreCientifico(tila,'tilia europea').
 
-modo(interno).
-modo(externo).
+%Explicación de los modos de preparación
+%*******************************************************************************************
+preparacionDeHiervas(cocimiento,'Hervir agua, poner la planta, hervir 10 min, reposar 5 min').
+preparacionDeHiervas(infusión, 'Hervir agua, colocar la planta, tapar y reposar 5 min').
+preparacionDeHiervas(maceración,'Triturar la planta, poner en agua y dejar remojar').
+preparacionDeHiervas(jarabe,'hervir 10 min, agregar azucar, agregar 10% de alcohol de caña y envasar').
+preparacionDeHiervas(tintura,'En envase con corcho poner la planta triturada y se agrega alcohol de caña y agua destilada, reposar en oscuridad una o dos semanas').
+preparacionDeHiervas(jugo,'exprimir la planta en un trapo limpio').
+preparacionDeHiervas(horchata,'se muelen las semillas agregando agua para formar una masilla, se cuela, se endulza y agrega agua para tomar').
 
-%--------------------------------------
-afecciones(parátitos).
-afecciones(hidropesía).
-afecciones(acumulación_de_toxinas).
-afecciones(problemas_renales).
-afecciones(estreñimiento).
-afecciones(peritonitis).
-afecciones(lombrices).
-afecciones(hernia_estrangulada).
-afecciones(caída_de_cabello).
-afecciones(caspa).
-afecciones(seborrea).
-afecciones(cólicos_infantles).
-afecciones(empachos).
-afecciones(conjuntivitis).
-afecciones(infecciones_vaginales).
-afecciones(baja_energía).
-afecciones(problemas_mestruales).
-afecciones(mala_digestión).
-afecciones(estrés).
-afecciones(partos_difíciles).
-afecciones(hijos_no_deseados).
-afecciones(mal_humor_en_ciclo_menstrual).
-afecciones(jaquecas).
-afecciones(ictericia).
-afecciones(catarro_vía_biliares).
-afecciones(cólico_hepático).
-afecciones(dispepsia_crónica).
-afecciones(sed).
-afecciones(indigestión_infantil).
-afecciones(hígado_intoxicado).
-afecciones(riñon_intoxicado).
-afecciones(cálculos_renales).
-afecciones(nerviosismo).
-afecciones(malestar_general).
-afecciones(problemas_de_sueño).
-afecciones(dolor_histérico_de_cabeza).
-afecciones(fiebre).
-afecciones(tos_espasmódica).
 
-%--------------------------------------
+
+%¿Cuales son las  enfermedades que curan las plantas?.
+%*******************************************************************************************
 elimina(retema,parátitos).
 elimina(retema,hidropesía).
 elimina(retema,acumulación_de_toxinas).
 elimina(retema,problemas_renales).
+
 elimina(ricino,estreñimiento).
 elimina(ricino,peritonitis).
 elimina(ricino,lombrices).
@@ -77,39 +56,45 @@ elimina(ricino,parátitos).
 elimina(ricino,caída_de_cabello).
 elimina(ricino,caspa).
 elimina(ricino,seborrea).
+
 elimina(rosal,estreñimiento).
 elimina(rosal,cólicos_infantles).
 elimina(rosal,empachos).
 elimina(rosal,conjuntivitis).
+
 elimina(romero,infecciones_vaginales).
 elimina(romero,baja_energía).
 elimina(romero,problemas_mestruales).
 elimina(romero,mala_digestión).
 elimina(romero,estrés).
 elimina(romero,caída_de_cabello).
+
 elimina(ruda,partos_difíciles).
 elimina(ruda,hijos_no_deseados).
 elimina(ruda,mal_humor_en_ciclo_menstrual).
 elimina(ruda,jaquecas).
 elimina(ruda,problemas_mestruales).
+
 elimina(simonillo,estreñimiento).
 elimina(simonillo,ictericia).
 elimina(simonillo,catarro_vía_biliares).
 elimina(simonillo,cólico_hepático).
 elimina(simonillo,dispepsia_crónica).
-elimina(tamarindo,parátitos).
-elimina(tamarindo,acumulación_de_toxinas).
+
 elimina(tamarindo,problemas_renales).
 elimina(tamarindo,estreñimiento).
 elimina(tamarindo,sed).
 elimina(tamarindo,empachos).
 elimina(tamarindo,indigestión_infantil).
+
 elimina(tabachín,hígado_intoxicado).
 elimina(tabachín,riñon_intoxicado).
 elimina(tabachín,hijos_no_deseados).
 elimina(tabachín,problemas_mestruales).
+
 elimina(taray,problemas_renales).
 elimina(taray,cálculos_renales).
+
 elimina(tila,nerviosismo).
 elimina(tila,malestar_general).
 elimina(tila,problemas_de_sueño).
@@ -119,91 +104,145 @@ elimina(tila,tos_espasmódica).
 elimina(tila,estrés).
 
 
-%--------------------------------------------------------
-
-modoDeAplicacion(parátitos,interno).
-modoDeAplicacion(hidropesía,interno).
-modoDeAplicacion(acumulación_de_toxinas,interno).
-modoDeAplicacion(problemas_renales,interno).
-modoDeAplicacion(estreñimiento,interno).
-modoDeAplicacion(peritonitis,interno).
-modoDeAplicacion(lombrices,interno).
-modoDeAplicacion(hernia_estrangulada,interno).
-modoDeAplicacion(caída_de_cabello,externo).
-modoDeAplicacion(caspa,externo).
-modoDeAplicacion(seborrea,externo).
-modoDeAplicacion(cólicos_infantles,interno).
-modoDeAplicacion(empachos,interno).
-modoDeAplicacion(conjuntivitis,externo).
-modoDeAplicacion(infecciones_vaginales,externo).
-modoDeAplicacion(baja_energía,interno).
-modoDeAplicacion(problemas_mestruales,interno).
-modoDeAplicacion(mala_digestión,interno).
-modoDeAplicacion(estrés,interno).
-modoDeAplicacion(partos_difíciles,interno).
-modoDeAplicacion(hijos_no_deseados,interno).
-modoDeAplicacion(mal_humor_en_ciclo_menstrual,interno).
-modoDeAplicacion(jaquecas,interno).
-modoDeAplicacion(ictericia,interno).
-modoDeAplicacion(catarro_vía_biliares,interno).
-modoDeAplicacion(cólico_hepático,interno).
-modoDeAplicacion(dispepsia_crónica,interno).
-modoDeAplicacion(sed,interno).
-modoDeAplicacion(indigestión_infantil,interno).
-modoDeAplicacion(hígado_intoxicado,interno).
-modoDeAplicacion(riñon_intoxicado,interno).
-modoDeAplicacion(cálculos_renales,interno).
-modoDeAplicacion(nerviosismo,interno).
-modoDeAplicacion(malestar_general,interno).
-modoDeAplicacion(problemas_de_sueño,interno).
-modoDeAplicacion(dolor_histérico_de_cabeza,interno).
-modoDeAplicacion(fiebre,interno).
-modoDeAplicacion(tos_espasmódica,interno).
+%¿Cuales son los modos de preparación de una planta en especifico?
+%*******************************************************************************************
+preparacionDePlanta(retema,'las ramas, flores y semillas se hierven').
+preparacionDePlanta(retema,'se hierve un muñado de cenizas de la planta en un litro de agua y se usa como té').
+preparacionDePlanta(ricino,'té').
+preparacionDePlanta(ricino,'se mezclan 40 gr de aceite de ricino y 10 gr de jugo de ortigas').
+preparacionDePlanta(rosal,'té').
+preparacionDePlanta(romero,'té').
+preparacionDePlanta(romero,'cigarillo').
+preparacionDePlanta(ruda,'té').
+preparacionDePlanta(simonillo,'se hirve toda la planta y se usa como té').
+preparacionDePlanta(tamarindo,'jugo').
+preparacionDePlanta(tabachín,'té').
+preparacionDePlanta(taray,'se remoja la madera toda la noche y se usa como agua de uso').
+preparacionDePlanta(tila,'té').
 
 
-%-----------------------------------------------
-
-modoDePreparacion(té,parátitos).
-modoDePreparacion(té,hidropesía).
-modoDePreparacion(té,acumulación_de_toxinas).
-modoDePreparacion(té,problemas_renales).
-modoDePreparacion(té,estreñimiento).
-modoDePreparacion(té,peritonitis).
-modoDePreparacion(té,lombrices).
-modoDePreparacion(té,hernia_estrangulada).
-modoDePreparacion(aceite,caída_de_cabello).
-modoDePreparacion(aceite,caspa).
-modoDePreparacion(aceite,seborrea).
-modoDePreparacion(infusión,cólicos_infantles).
-modoDePreparacion(infusión,empachos).
-modoDePreparacion(infusión,conjuntivitis).
-modoDePreparacion(té,infecciones_vaginales).
-modoDePreparacion(té,baja_energía).
-modoDePreparacion(té,problemas_mestruales).
-modoDePreparacion(té,mala_digestión).
-modoDePreparacion(té,estrés).
-modoDePreparacion(té,partos_difíciles).
-modoDePreparacion(té,hijos_no_deseados).
-modoDePreparacion(té,mal_humor_en_ciclo_menstrual).
-modoDePreparacion(té,jaquecas).
-modoDePreparacion(té,ictericia).
-modoDePreparacion(té,catarro_vía_biliares).
-modoDePreparacion(té,cólico_hepátic).
-modoDePreparacion(té,dispepsia_crónica).
-modoDePreparacion(agua,sed).
-modoDePreparacion(agua,indigestión_infantil).
-modoDePreparacion(té,hígado_intoxicado).
-modoDePreparacion(té,riñon_intoxicado).
-modoDePreparacion(remojo,cálculos_renales).
-modoDePreparacion(té,nerviosismo).
-modoDePreparacion(té,malestar_general).
-modoDePreparacion(té,problemas_de_sueño).
-modoDePreparacion(té,dolor_histérico_de_cabeza).
-modoDePreparacion(té,fiebre).
-modoDePreparacion(té,tos_espasmódica).
+%Modos de preparación de cada planta
+%*******************************************************************************************
+uso(retema,cocimiento).
+uso(ricino,infusión).
+uso(rosal,infusión).
+uso(romero,cocimiento).
+uso(ruda,infusión).
+uso(simonillo,cocimiento).
+uso(tamarindo,jugo).
+uso(tabachín,infusión).
+uso(taray,maceración).
+uso(tila,cocimiento).
 
 
-%---------------------------------------------------------------
+%Glosario de efectos de las plantas
+%*******************************************************************************************
+efectoPlanta(abortiva,'evita la formación del feto').
+efectoPlanta(afrodisíaca,'exita el apetito sexual').
+efectoPlanta(analgésica,'quita o modera el dolor').
+efectoPlanta(anestésica,'insensibiliza el cuerpo').
+efectoPlanta(antidiarreica,'controla diarreas').
+efectoPlanta(antiespasmódica,'controla espasmos nerviosos').
+efectoPlanta(antiflogística,'actua contra inflamaciones').
+efectoPlanta(antipirética,'quita la fiebre').
+efectoPlanta(antiséptica,'mata los tejidos').
+efectoPlanta(aperitiva,'produce apetito').
+efectoPlanta(astringente,'hace contraer los tejidos').
+efectoPlanta(carminativa,'evita la formación de gases o provoca su expulsion').
+efectoPlanta(colagoga,'ayuda a expulsar la bilis').
+efectoPlanta(depurativa,'limpia y purifica la sangre').
+efectoPlanta(diaforética,'provoca sudar').
+efectoPlanta(digestiva,'favorece la digestion').
+efectoPlanta(diurética,'provoca la orina').
+efectoPlanta(emética,'provoca nauseas y vomitos').
+efectoPlanta(emenagoga,'activa la menstruacion').
+efectoPlanta(estupefciente,'aquieta, duerme o atonta').
+efectoPlanta(estimulante,'aumenta el estado de energía').
+efectoPlanta(expectorante,'expulsa las flemas').
+efectoPlanta(hemostática,'corta o detiene hemorragias').
+efectoPlanta(hepática,'ayuda al higado').
+efectoPlanta(laxante,'purga sin provocar diarrea').
+efectoPlanta(pectoral,'ayuda al pecho').
+efectoPlanta(refrescante,'alivia la sed').
+efectoPlanta(sedante,'calma dolores intestinales').
+efectoPlanta(tónica,'da fuerza al organismo').
+efectoPlanta(tóxica,'es venenosa').
+efectoPlanta(vermífuga,'expulsa gusanos intestinales').
+efectoPlanta(vulneraria,'cura llagas y heridas').
+
+
+%*******************************************************************************************
+%¿Cuales son las acciones o efectos que tienen las plantas?    
+efecto(retema,laxante).
+efecto(retema,diurética).
+efecto(retema,depurativa).
+
+efecto(ricino,digestiva).
+efecto(ricino,laxante).
+efecto(ricino,astringente).
+efecto(ricino,vermífuga).
+efecto(ricino,tóxica).
+
+efecto(rosal,laxante).
+efecto(rosal,astringente).
+efecto(rosal,digestiva).
+
+
+efecto(romero,estimulante).
+efecto(romero,emenagoga).
+efecto(romero,digestiva).
+
+
+efecto(ruda,emenagoga).
+efecto(ruda,abortiva).
+efecto(ruda,antiespasmódica).
+efecto(ruda,sedante).
+
+
+efecto(simonillo,digestiva).
+efecto(simonillo,colagoga).
+efecto(simonillo,hepática).
+
+
+efecto(tamarindo,laxante).
+efecto(tamarindo,diurética).
+efecto(tamarindo,refrescante).
+efecto(tamarindo,digestiva).
+
+
+efecto(tabachín,depurativa).
+efecto(tabachín,abortiva).
+efecto(tabachín,emenagoga).
+efecto(tabachín,colagoga).
+
+efecto(taray,diurética).
+
+efecto(tila,sedante).
+efecto(tila,analgésica).
+efecto(tila,antipirética).
+efecto(tila,antiespasmódica).
+
+
+%¿Qué elementos tiene una planta en especifica?
+%*******************************************************************************************
+
+parteDePlanta(retema, 'ramas, flores, semillas').
+parteDePlanta(ricino, 'semillas').
+parteDePlanta(rosal, 'petalos, pistilos, caliz').
+parteDePlanta(romero, 'hojas, flores, aceites esenciales').
+parteDePlanta(ruda, 'hojas').
+parteDePlanta(simonillo, 'desconocido').
+parteDePlanta(tamarindo, 'hojas, flor, fruto, tronco, ramas, raíz.').
+parteDePlanta(tabachín, 'hojas, flores, ramas, fruto').
+parteDePlanta(taray, 'flores, ramas').
+parteDePlanta(tila, 'flores').
+
+%¿Cual es el origen de una planta?
+%******************************************************************************************
+
+
+
+%*******************************************************************************************
 
 aplicacion(parátitos,interno,té).
 aplicacion(hidropesía,interno,té).
@@ -232,7 +271,7 @@ aplicacion(ictericia,interno,té).
 aplicacion(catarro_vía_biliares,interno,té).
 aplicacion(cólico_hepático,interno,té).
 aplicacion(dispepsia_crónica,interno,té).
-aplicacion(sed,interno,agua).
+aplicacion(sed,interno,jugo).
 aplicacion(indigestión_infantil,interno,agua).
 aplicacion(hígado_intoxicado,interno,té).
 aplicacion(riñon_intoxicado,interno,té).
@@ -253,16 +292,8 @@ recetar(AFECCIONES,PLANTA,MODO,PREPARACION):-elimina(PLANTA,AFECCIONES),aplicaci
 %recetar(partos_difíciles,X,Z,R).
 %recetar(X,tila,Z,R).
 
-alivia(AFECCIONES,PLANTA):-elimina(PLANTA,AFECCIONES).
-%alivia(X,tila).
-%alivia(X,tamarindo).
-%alivia(ictericia,X).
-%alivia(problemas_mestruales,X).
 
-problemaInterno(AFECCIONES):-modoDeAplicacion(AFECCIONES,interno).
-problemaExterno(AFECCIONES):-modoDeAplicacion(AFECCIONES,externo).
-%problemaInterno(X).
-%problemaExterno(Y).
+
 
 usar(AFECCION,PREPARACION):-modoDePreparacion(PREPARACION,AFECCION).
 %usar(estrés,X).
@@ -278,3 +309,20 @@ remojo(X):-modoDePreparacion(remojo,X).
 
 ingerir(X,Z,Y):-elimina(X,Z),modoDeAplicacion(Z,Y).
 %ingerir(ruda,X,Y).
+
+
+
+%funciones extras sin probar---------------------------------------------------------------------------------------
+resumen_planta(X,Y,Z):-nombreCientifico(X,Y),elimina(X,Z).
+modo_uso(X,Z,W,Y):-uso(X,Y),preparacionDeHiervas(Y,Z),preparacionDePlanta(X,W).
+efectoNombreplanta(Y,X):-efecto(X,Y),efectoPlanta(Y,_).
+efectoXPlanta(Z,X):-efecto(X,Y),efectoPlanta(Y,Z).
+
+
+%*******************************************************************************************
+%¿Cuales son las plantas que curan el herpes?
+plantasCuranHerpes(Planta):-elimina(Planta,herpes).
+
+%¿Qué plantas son analgésicas?
+plantasAnalgesica(Planta):-efecto(Planta,analgésica).
+
